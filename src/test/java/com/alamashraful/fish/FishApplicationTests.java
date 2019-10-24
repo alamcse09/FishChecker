3042388c3da6debfb81385bc6fe49e0ed4727de6
Ashraful Alam <alam.cse09@gmail.com>
2019-10-24 12:17:26 +0600

    Initial commit

---
+++ src/test/java/com/alamashraful/fish/FishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamashraful.fish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
