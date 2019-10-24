4abf03110b425061cfa61941fd16472bf368609a
Ashraful Alam <alam.cse09@gmail.com>
2019-10-24 16:02:53 +0600

    Background added

--- src/main/java/com/alamashraful/fish/security/SecurityConfig.java
+++ src/main/java/com/alamashraful/fish/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.alamashraful.fish.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
